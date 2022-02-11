80714f8c46b4f0796cbde69fdfa67f5f76db40fc
azamov_dev <azamov.dev@gmail.com>
2022-02-12 01:22:32 +0500

    project setup

--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,14 @@
-import express from "express";
-import config from "config";
-import log from "./logger";
-import connect from "./db/connect";
-import routes from "./routes";
-import { deserializeUser } from "./middleware";
+import express from "express"
+import config from "config"
 
-const port = config.get("port") as number;
-const host = config.get("host") as string;
+const port = config.get("port") as number
+const host = config.get("host") as string
 
-const app = express();
-app.use(deserializeUser);
+const app = express()
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 
 app.listen(port, host, () => {
-  log.info(`Server listing at http://${host}:${port}`);
-
-  connect();
-
-  routes(app);
-});
+  console.log(`Server running on https://${host}/${port} `)
+})
--- src/middleware/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as deserializeUser } from "./deserializeUser";
-export { default as requiresUser } from "./requiresUser";
-export { default as validateRequest } from "./validateRequest";
